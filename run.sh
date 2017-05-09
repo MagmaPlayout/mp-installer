d051fa0846f5899a7fcb94c530bedbc8168ab794
Iber <iberparodisiri@gmail.com>
2017-05-08 22:28:27 -0300

    Added core developer mode to the run script (only starts up melted and redis)

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-figlet "Magma Playout"
-
 # Overwrite these configurations:
 REDIS_PATH=~/Documentos/PROYECTO_FINAL/Herramientas/redis-3.2.4/src
 MELTED_PATH=~/melted/20160711
@@ -18,17 +15,35 @@ PLAYOUT_UI=$BASE_PATH"gui/mp-ui-playout/"
 # Commands
 MELTED_SERVER="/bin/bash -c './start-melted-server; exec /bin/bash -i'" # Keeps terminal open
 REDIS_SERVER="redis-server"
+REDIS_CLIENT="redis-cli"
 NODE_SERVER="node server.js"
 ANGULAR_SERVER="ng serve"
 JAVA_CORE="java -jar mp-core.jar"
 
-# Runs gnome-terminal with each app on a separate tab
-gnome-terminal --maximize \
-		--tab --working-directory="$REDIS_PATH" -e "$REDIS_SERVER" --title="redis server" \
-		--tab --working-directory="$MELTED_PATH" -e "$MELTED_SERVER" --title="melted" \
-		--tab --working-directory="$CORE_API" -e "$NODE_SERVER" --title="core api" \
-		--tab --working-directory="$ADMIN_API" -e "$NODE_SERVER" --title="admin api" \
-		--tab --working-directory="$PLAYOUT_API" -e "$NODE_SERVER" --title="playout api" \
-		--tab --working-directory="$CORE" -e "$JAVA_CORE" --title="core" \
-		--tab --working-directory="$PLAYOUT_UI" -e "$ANGULAR_SERVER" --title="frontend" \
-		&>/dev/null
+coredevel=$1
+if [[ -n "$coredevel" ]]; then
+	figlet "MP - Core Developer"
+	
+	# Runs only core related stuff. For developing.
+	gnome-terminal --maximize \
+			--tab --working-directory="$REDIS_PATH" -e "$REDIS_SERVER" --title="redis server" \
+			--tab --working-directory="$REDIS_PATH" -e "$REDIS_CLIENT" --title="redis client1" \
+			--tab --working-directory="$REDIS_PATH" -e "$REDIS_CLIENT" --title="redis client2" \
+			--tab --working-directory="$MELTED_PATH" -e "$MELTED_SERVER" --title="melted" \
+			&>/dev/null
+else	
+	figlet "Magma Playout"
+	
+	# Runs gnome-terminal with each app on a separate tab
+	gnome-terminal --maximize \
+			--tab --working-directory="$REDIS_PATH" -e "$REDIS_SERVER" --title="redis server" \
+			--tab --working-directory="$MELTED_PATH" -e "$MELTED_SERVER" --title="melted" \
+			--tab --working-directory="$CORE_API" -e "$NODE_SERVER" --title="core api" \
+			--tab --working-directory="$ADMIN_API" -e "$NODE_SERVER" --title="admin api" \
+			--tab --working-directory="$PLAYOUT_API" -e "$NODE_SERVER" --title="playout api" \
+			--tab --working-directory="$CORE" -e "$JAVA_CORE" --title="core" \
+			--tab --working-directory="$PLAYOUT_UI" -e "$ANGULAR_SERVER" --title="frontend" \
+			&>/dev/null
+fi			
+			
+
