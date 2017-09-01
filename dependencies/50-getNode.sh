3b73e3d5abaeb927b8a811509629610d542280f8
Iber <iberparodisiri@gmail.com>
2017-09-01 00:17:39 -0300

    Added Angular dependency

--- dependencies/50-getNode.sh
+++ dependencies/50-getNode.sh
@@ -5,7 +5,9 @@ wget -c https://github.com/nodejs/node/archive/v6.11.2.tar.gz
 tar -xf v6.11.2.tar.gz
 cd node-6.11.2
  ./configure
+ echo "Building Node..."
  make -j4
  sudo make install
 
-
+echo "Installing Angular..."
+npm install -g @angular/cli
