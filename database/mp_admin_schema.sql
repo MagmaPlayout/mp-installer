41004786e6b4c29ff5cd0854d5b3d5bfbbd42e98
Iber <iberparodisiri@gmail.com>
2017-10-14 11:55:56 -0300

    Exported SQL with mysql workbench. Added new fields to playoutLog table

--- database/mp_admin_schema.sql
+++ database/mp_admin_schema.sql
@@ -1,151 +1,172 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `mp_admin` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mp_admin`;
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: mp_admin
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB-1
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema mp_admin
--- -----------------------------------------------------
+--
+-- Table structure for table `Action`
+--
 
--- -----------------------------------------------------
--- Schema mp_admin
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mp_admin` DEFAULT CHARACTER SET latin1 ;
-USE `mp_admin` ;
-
--- -----------------------------------------------------
--- Table `mp_admin`.`Action`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mp_admin`.`Action` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(255) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `Action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `PlayoutLog`
+--
 
--- -----------------------------------------------------
--- Table `mp_admin`.`Role`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mp_admin`.`Role` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+DROP TABLE IF EXISTS `PlayoutLog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PlayoutLog` (
+  `idRawMedia` int(11) NOT NULL,
+  `filter` varchar(45) DEFAULT NULL,
+  `sketch` varchar(45) DEFAULT NULL,
+  `starttime` datetime NOT NULL,
+  `endtime` datetime NOT NULL,
+  `pieceName` varchar(255) NOT NULL,
+  `resolution` varchar(45) NOT NULL,
+  `duration` varchar(45) NOT NULL,
+  `piecePath` varchar(255) NOT NULL,
+  `frameCount` int(11) NOT NULL,
+  `frameRate` int(11) NOT NULL,
+  PRIMARY KEY (`starttime`),
+  KEY `fk_PlayoutLog_1_idx` (`idRawMedia`),
+  CONSTRAINT `fk_PlayoutLog_RawMedia` FOREIGN KEY (`idRawMedia`) REFERENCES `RawMedia` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `RawMedia`
+--
 
--- -----------------------------------------------------
--- Table `mp_admin`.`RoleActions`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mp_admin`.`RoleActions` (
-  `idRole` INT(11) NOT NULL,
-  `idAction` INT(11) NOT NULL,
-  PRIMARY KEY (`idRole`, `idAction`),
-  INDEX `fk_RoleActions_Action_idx` (`idAction` ASC),
-  CONSTRAINT `fk_RoleActions_Action`
-    FOREIGN KEY (`idAction`)
-    REFERENCES `mp_admin`.`Action` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_RoleActions_Role`
-    FOREIGN KEY (`idRole`)
-    REFERENCES `mp_admin`.`Role` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `mp_admin`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mp_admin`.`User` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `surname` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
-  `phone` VARCHAR(100) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `RawMedia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RawMedia` (
+  `id` int(11) NOT NULL,
+  `idSupplier` int(11) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = latin1;
+  KEY `fk_RawMedia_1_idx` (`idSupplier`),
+  CONSTRAINT `fk_RawMedia_Supplier` FOREIGN KEY (`idSupplier`) REFERENCES `Supplier` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `Role`
+--
 
--- -----------------------------------------------------
--- Table `mp_admin`.`UserActions`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mp_admin`.`UserActions` (
-  `idUser` INT(11) NOT NULL,
-  `idAction` INT(11) NOT NULL,
-  PRIMARY KEY (`idUser`, `idAction`),
-  INDEX `fk_UserActions_Action_idx` (`idAction` ASC),
-  CONSTRAINT `fk_UserActions_Action`
-    FOREIGN KEY (`idAction`)
-    REFERENCES `mp_admin`.`Action` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_UserActions_User`
-    FOREIGN KEY (`idUser`)
-    REFERENCES `mp_admin`.`User` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+DROP TABLE IF EXISTS `Role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `mp_admin`.`Supplier`
--- -----------------------------------------------------
-CREATE TABLE `mp_admin`.`Supplier` (
- `id` INT NOT NULL AUTO_INCREMENT,
- `phone` VARCHAR(45) NULL,
- `name` VARCHAR(45) NOT NULL,
- `email` VARCHAR(45) NULL,
- PRIMARY KEY (`id`));
+--
+-- Table structure for table `RoleActions`
+--
 
--- -----------------------------------------------------
--- Table `mp_admin`.`RawMedia`
--- -----------------------------------------------------
-CREATE TABLE `mp_admin`.`RawMedia` (
- `id` INT NOT NULL,
- `idSupplier` INT NULL,
- `location` VARCHAR(255) NULL,
- PRIMARY KEY (`id`),
- INDEX `fk_RawMedia_1_idx` (`idSupplier` ASC),
- CONSTRAINT `fk_RawMedia_Supplier`
-   FOREIGN KEY (`idSupplier`)
-   REFERENCES `mp_admin`.`Supplier` (`id`)
-   ON DELETE NO ACTION
-   ON UPDATE NO ACTION);
+DROP TABLE IF EXISTS `RoleActions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RoleActions` (
+  `idRole` int(11) NOT NULL,
+  `idAction` int(11) NOT NULL,
+  PRIMARY KEY (`idRole`,`idAction`),
+  KEY `fk_RoleActions_Action_idx` (`idAction`),
+  CONSTRAINT `fk_RoleActions_Action` FOREIGN KEY (`idAction`) REFERENCES `Action` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_RoleActions_Role` FOREIGN KEY (`idRole`) REFERENCES `Role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `mp_admin`.`PlayoutLog`
--- -----------------------------------------------------
-CREATE TABLE `mp_admin`.`PlayoutLog` (
- `idRawMedia` INT NOT NULL,
- `filter` VARCHAR(45) NULL,
- `sketch` VARCHAR(45) NULL,
- `starttime` DATETIME NOT NULL,
- `endtime` DATETIME NOT NULL,
- PRIMARY KEY (`starttime`),
- INDEX `fk_PlayoutLog_1_idx` (`idRawMedia` ASC),
- CONSTRAINT `fk_PlayoutLog_RawMedia`
-   FOREIGN KEY (`idRawMedia`)
-   REFERENCES `mp_admin`.`RawMedia` (`id`)
-   ON DELETE NO ACTION
-   ON UPDATE NO ACTION);
+--
+-- Table structure for table `Supplier`
+--
 
+DROP TABLE IF EXISTS `Supplier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `UserActions`
+--
+
+DROP TABLE IF EXISTS `UserActions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `UserActions` (
+  `idUser` int(11) NOT NULL,
+  `idAction` int(11) NOT NULL,
+  PRIMARY KEY (`idUser`,`idAction`),
+  KEY `fk_UserActions_Action_idx` (`idAction`),
+  CONSTRAINT `fk_UserActions_Action` FOREIGN KEY (`idAction`) REFERENCES `Action` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_UserActions_User` FOREIGN KEY (`idUser`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-14 11:54:17
