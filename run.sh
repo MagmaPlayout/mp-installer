cc6b73f979929ed8af785ad20a69eb5eac72469c
Iber <iberparodisiri@gmail.com>
2017-11-04 13:23:36 -0300

    Made run script retro compatible with previous tmux versions

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 # Relative paths (no need to modify)
 BASE_PATH="$(pwd)/magma-playout/"
-MELTED_PATH=$BASE_PATH"/core/melted/installation/"
+MELTED_PATH=$BASE_PATH"core/melted/installation/"
 MELTED_STATUS_PATH=$BASE_PATH"core/mp-melted-status/dist/"
 CORE_API=$BASE_PATH"core/mp-core-api/"
 CORE=$BASE_PATH"core/mp-core/dist/"
 ADMIN_API=$BASE_PATH"store/mp-admin-api/"
 PLAYOUT_API=$BASE_PATH"store/mp-playout-api/"
 PLAYOUT_UI=$BASE_PATH"gui/mp-ui-playout/"
-REDIS_PATH=$BASE_PATH"/core/redis/src"
+REDIS_PATH=$BASE_PATH"core/redis/src/"
 
 # Commands
 MELTED_SERVER="./start-melted-server"
@@ -35,14 +35,14 @@ else
 	figlet "Magma Playout"
 	echo "Creating tmux session..."
 	tmux -f dependencies/sources/tmux.conf new -s magma -d 
-	tmux new-window -n redis  -t magma -c $REDIS_PATH $REDIS_SERVER
-	tmux new-window -n melted -t magma -c $MELTED_PATH $MELTED_SERVER 
-	tmux new-window -n status -t magma -c $MELTED_STATUS_PATH $MELTED_STATUS
-	tmux new-window -n coreapi  -t magma -c $CORE_API $NODE_SERVER
-	tmux new-window -n adminapi -t magma -c $ADMIN_API $NODE_SERVER
-	tmux new-window -n playoutapi -t magma -c $PLAYOUT_API $NODE_SERVER
-	tmux new-window -n core -t magma -c $CORE $JAVA_CORE
-	tmux new-window -n gui  -t magma -c $PLAYOUT_UI $ANGULAR_SERVER
+	tmux new-window -n redis  -t magma -c "$REDIS_PATH" "$REDIS_SERVER"
+	tmux new-window -n melted -t magma -c "$MELTED_PATH" "$MELTED_SERVER"
+	tmux new-window -n status -t magma -c "$MELTED_STATUS_PATH" "$MELTED_STATUS"
+	tmux new-window -n coreapi  -t magma -c "$CORE_API" "$NODE_SERVER"
+	tmux new-window -n adminapi -t magma -c "$ADMIN_API" "$NODE_SERVER"
+	tmux new-window -n playoutapi -t magma -c "$PLAYOUT_API" "$NODE_SERVER"
+	tmux new-window -n core -t magma -c "$CORE" "$JAVA_CORE"
+	tmux new-window -n gui  -t magma -c "$PLAYOUT_UI" "$ANGULAR_SERVER"
 	echo "Done! now run: "
 	echo "    tmux attach-session -t magma"
 	echo " use (Ctrl+B)(r) shortcut to respawn a killed command"
