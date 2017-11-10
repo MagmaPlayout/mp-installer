77483b1a9964740928a15c81e8158c58f4ef942f
Iber <iberparodisiri@gmail.com>
2017-11-09 21:13:34 -0300

    Bugfix on time filter

--- database/initial_data.sql
+++ database/initial_data.sql
@@ -9,7 +9,7 @@ INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) VALUES ('4', 'media'
 
 
 INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('2', 'grayscale', 'Turns color output into a grayscale');
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('4', 'time', 'Displays the current time over the output');
+INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('4', 'dynamictext', 'Displays the current time over the output');
 INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('3', 'oldfilm', 'Applies an old film effect');
 INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('1', 'dust', 'Applies dust particles over the output');
 INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('5', 'watermark', 'Displays an image over the output');
