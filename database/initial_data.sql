bc24b54b342b3e2118dc99f0ec7bba9d782cab07
Iber <iberparodisiri@gmail.com>
2017-11-18 10:30:44 -0300

    Clean up initial_data.sql

--- database/initial_data.sql
+++ database/initial_data.sql
@@ -1,8 +1,9 @@
 -- Creates ROLES
-INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) VALUES ('1', 'administrator', 'All permissions');
-INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) VALUES ('2', 'operator', 'Calendar and live mode access');
-INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) VALUES ('3', 'analyst', 'Access only reports');
-INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) VALUES ('4', 'dataEntry', 'Access only media ABM');
+INSERT INTO `mp_admin`.`Role` (`id`, `name`, `description`) 
+VALUES 	('1', 'administrator', 'All permissions'),
+		('2', 'operator', 'Calendar and live mode access'),
+		('3', 'analyst', 'Access only reports'),
+		('4', 'dataEntry', 'Access only media ABM');
 
 -- Creates ACTIONS
 INSERT INTO `mp_admin`.`Action`
@@ -16,23 +17,24 @@ VALUES  (1,'View Scheduler Mode',NULL),
 INSERT INTO `mp_admin`.`RoleActions` (`idRole`, `idAction`)
 VALUES (1,1), (1,2), (1,3), (1,4), (1,5), (2,1), (2,2), (3,3), (4,4);
 
-
 -- Creates ADMIN user
-INSERT INTO User (name, surname, username, password, idRole)
-VALUES ("admin", "admin", "admin", "admin", 1);
+INSERT INTO `mp_admin`.`User` (`name`, `surname`, `username`, `password`, `idRole`)
+VALUES ('administrator', ' ', 'admin', 'admin', 1);
 
 -- Creates Filters
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('2', 'grayscale', 'Turns color output into a grayscale');
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('4', 'dynamictext', 'Displays the current time over the output');
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('3', 'oldfilm', 'Applies an old film effect');
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('1', 'dust', 'Applies dust particles over the output');
-INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) VALUES ('5', 'watermark', 'Displays an image over the output');
+INSERT INTO `mp_playout`.`Filter` (`id`, `name`, `description`) 
+VALUES 	('2', 'grayscale', 'Turns color output into a grayscale'),
+		('4', 'dynamictext', 'Displays the current time over the output'),
+		('3', 'oldfilm', 'Applies an old film effect'),
+		('1', 'dust', 'Applies dust particles over the output'),
+		('5', 'watermark', 'Displays an image over the output');
 
-INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) VALUES ('1', '1', 'mlt_service', 'id of filter');
-INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) VALUES ('2', '2', 'mlt_service', 'id of filter');
-INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) VALUES ('3', '3', 'mlt_service', 'id of filter');
-INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) VALUES ('4', '4', 'mlt_service', 'id of filter');
-INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) VALUES ('5', '5', 'mlt_service', 'id of filter');
+INSERT INTO `mp_playout`.`FilterArgs` (`id`, `filterId`, `key`, `description`) 
+VALUES  ('1', '1', 'mlt_service', 'id of filter'),
+		('2', '2', 'mlt_service', 'id of filter'),
+		('3', '3', 'mlt_service', 'id of filter'),
+		('4', '4', 'mlt_service', 'id of filter'),
+		('5', '5', 'mlt_service', 'id of filter');
 
 
 
